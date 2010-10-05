de4cbd5f877ed1c36f2d3f8f569fbd3ba341652e
Jannis Hermanns <jannis@moviepilot.de>
2010-10-05 15:46:02 +0200

    Half of the httperf output spec done

--- lib/httperf.rb
+++ lib/httperf.rb
@@ -9,26 +9,28 @@ module Httperf
 
       case line
       when /^Total: connections (\d+) requests (\d+) replies (\d+) test-duration (\d+\.\d+) s/ then
-        res['conns']    = $1
-        res['requests'] = $2
-        res['replies']  = $3
-        res['duration'] = $4
+        res['conns']    = $1.to_i
+        res['requests'] = $2.to_i
+        res['replies']  = $3.to_i
+        res['duration'] = $4.to_f
       when /^Connection rate: (\d+\.\d)[^\d]+(\d+\.\d)[^\d]+(\d+)[^\d]/ then 
-        res['conn/s']               = $1
-        res['ms/connection']        = $2
-        res['concurrent conns max'] = $3
+        res['conn/s']               = $1.to_f
+        res['ms/connection']        = $2.to_f
+        res['concurrent connections max'] = $3.to_i
       when /^Connection time .*min (\d+\.\d) avg (\d+\.\d) max (\d+\.\d) median (\d+\.\d) stddev (\d+\.\d)/ then
-        res['conn time min']    = $1
-        res['conn time avg']    = $2
-        res['conn time max']    = $3
-        res['conn time median'] = $4
-        res['conn time stddev'] = $5
-      when /^Request rate: (\d+\.\d)/ then res['req/s'] = $1
+        res['conn time min']    = $1.to_f
+        res['conn time avg']    = $2.to_f
+        res['conn time max']    = $3.to_f
+        res['conn time median'] = $4.to_f
+        res['conn time stddev'] = $5.to_f
+      when /^Request rate: (\d+\.\d)[^)]+\((\d+\.\d)/ then
+        res['req/s'] = $1.to_f
+        res['ms/req'] = $2.to_f
       when /^Reply rate .*min (\d+\.\d) avg (\d+\.\d) max (\d+\.\d) stddev (\d+\.\d)/ then
-        res['replies/s min'] = $1
-        res['replies/s avg'] = $2
-        res['replies/s max'] = $3
-        res['replies/s stddev'] = $4
+        res['replies/s min']    = $1.to_f
+        res['replies/s avg']    = $2.to_f
+        res['replies/s max']    = $3.to_f
+        res['replies/s stddev'] = $4.to_f
       when /^Reply time .* response (\d+\.\d)/ then res['reply time'] = $1
       when /^Reply status.+ 1xx=(\d+) 2xx=(\d+) 3xx=(\d+) 4xx=(\d+) 5xx=(\d+)/ then
         res['status 1xx'] = $1 
--- spec/lib/httperf_spec.rb
+++ spec/lib/httperf_spec.rb
@@ -15,6 +15,40 @@ describe Httperf do
       @result['replies'].should  == 300
       @result['duration'].should == 50.354
     end
+
+    it "should parse the 'Connection rate' line correctly" do
+      @result['conn/s'].should == 9.9
+      @result['ms/connection'].should == 100.7
+      @result['concurrent connections max'].should == 8
+    end
+    
+    it "should parse the 'Connection time' line correctly" do
+      @result['conn time min'].should    == 449.7
+      @result['conn time avg'].should    == 465.1
+      @result['conn time max'].should    == 2856.6
+      @result['conn time median'].should == 451.5
+      @result['conn time stddev'].should == 132.1
+    end
+
+    it "should parse the second 'Connection time' line correctly"
+    it "should parse the 'Connection length' line correctly"
+
+    it "should parse the 'Request rate' line correctly" do
+      @result['req/s'].should == 9.9
+      @result['ms/req'].should == 100.7
+    end
+
+    it "should parse the 'Request size' line correctly"
+
+    it "should parse the 'Reply rate' line correctly" do
+      @result['replies/s min'].should    == 9.2
+      @result['replies/s avg'].should    == 9.9
+      @result['replies/s max'].should    == 10.0
+      @result['replies/s stddev'].should == 0.3
+    end
+
+    it "should parse the 'Reply time' line correctly" do
+    end
   end
 
 end
